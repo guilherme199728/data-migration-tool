608fff36892fc7ff0a9f9099ba1f349a0b2be98f
Guilherme Alves <guilherme199728@gmail.com>
2022-02-20 04:30:30 -0300

    inserindo dados manuais para testes iniciais

--- src/main/java/com/br/migrationTool/Principal.java
+++ src/main/java/com/br/migrationTool/Principal.java
@@ -1,21 +1,26 @@
 package com.br.migrationTool;
 
 import com.br.migrationTool.data.connection.ConnetionOracleJDBC;
-import com.br.migrationTool.data.dao.TableReferencesDao;
+import com.br.migrationTool.vo.MigrationVo;
 
-import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Principal {
+
     public static void main(String[] args) throws SQLException {
 
         ConnetionOracleJDBC.initDatabaseConnectionPool();
-        Connection con = ConnetionOracleJDBC.getConnectionProd();
 
-        TableReferencesDao.getParentTablesFromTableName("PROD", "ACCOUNT", con);
-        TableReferencesDao.getAllNamesColunsTableFromTableName("ACCOUNT", con);
-        TableReferencesDao.getChildrenTablesFromTableName("PROD", "ACCOUNT", con);
-        TableReferencesDao.getPrimaryKeyNamesFromTableName("PROD", "ACCOUNT", con);
+        String initialTableNameForMigration = "ACCOUNT";
+        List<String> primaryKeysForMigration = new ArrayList<>();
+        primaryKeysForMigration.add("1");
+        primaryKeysForMigration.add("2");
+        primaryKeysForMigration.add("3");
+        primaryKeysForMigration.add("3");
+
+        MigrationVo.setListMigration(initialTableNameForMigration, primaryKeysForMigration);
 
         ConnetionOracleJDBC.closeDataBaseConnectionPool();
 
